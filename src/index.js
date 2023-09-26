1e7fef4832bd9fa29e42ebc2c4e4e4cbca903395
prashant656 <prashantaupreti1@gmail.com>
2023-09-26 17:07:36 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+app.get("/aboutMe", (req, res) => {
+    res.render("pages/aboutMe", {
+      title: "About EJS",
+      details: [
+        "MUGIIII EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.",
+        "EJS is compatible with Express for back-end use as it hooks into the View engine architecture that Express provides and lets you render web pages to the client with res.render() in Express.",
+      ],
+    });
+  });
+  app.get("/projects",(req,res)=>{
+    res.render("pages/projects");
+  })
+  app.get("/contact",(req,res)=>{
+    res.render("pages/contact");
+  })
+  app.get("/services",(req,res)=>{
+    res.render("pages/services");
+  })
+  
+app.listen(4000);
