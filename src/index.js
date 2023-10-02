b3f08a05009251a6460d2ff26148d6101cf6eac9
prashant656 <prashantaupreti1@gmail.com>
2023-10-02 19:37:16 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.get("/aboutMe", (req, res) => {
     res.render("pages/aboutMe", {
       title: "About EJS",
       details: [
-        "MUGIIII EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.",
+        "EJS is a simple templating language that lets you generate HTML markup with plain JavaScript.",
         "EJS is compatible with Express for back-end use as it hooks into the View engine architecture that Express provides and lets you render web pages to the client with res.render() in Express.",
       ],
     });
